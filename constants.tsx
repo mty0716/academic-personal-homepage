02655b36f5fc74f6b24d1a2f32fade976a07a4e1
Tianyue Ma <tianyue.ma@example.com>
2025-10-15 09:33:31 +0800

    fix avatar image path for GitHub Pages deployment

--- constants.tsx
+++ constants.tsx
@@ -15,7 +15,7 @@ export const CONTENT = {
           github: "https://github.com/",
           twitter: "https://twitter.com/",
       },
-      imageUrl: "/profile.jpg"
+      imageUrl: process.env.NODE_ENV === 'production' ? '/academic-personal-homepage/profile.jpg' : '/profile.jpg'
     },
     aboutMe: {
         title: "About Me",
@@ -131,7 +131,7 @@ export const CONTENT = {
           github: "https://github.com/",
           twitter: "https://twitter.com/",
       },
-      imageUrl: "/profile.jpg"
+      imageUrl: process.env.NODE_ENV === 'production' ? '/academic-personal-homepage/profile.jpg' : '/profile.jpg'
     },
     aboutMe: {
         title: "关于我",
